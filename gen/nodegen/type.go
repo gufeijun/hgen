259b9e360c2e271217566ea8ee2d715b013ded7d
gufeijun <gugoings@gmail.com>
2021-10-28 18:08:51 +0800

    add float support for rpc-node

--- gen/nodegen/type.go
+++ gen/nodegen/type.go
@@ -8,25 +8,29 @@ import (
 )
 
 var unmarshalMap = map[string]string{
-	"int8":   "readInt8",
-	"int16":  "readInt16LE",
-	"int32":  "readInt32LE",
-	"int64":  "readBigInt64LE",
-	"uint8":  "readUInt8",
-	"uint16": "readUInt16LE",
-	"uint32": "readUInt32LE",
-	"uint64": "readBigUInt64LE",
+	"int8":    "readInt8",
+	"int16":   "readInt16LE",
+	"int32":   "readInt32LE",
+	"int64":   "readBigInt64LE",
+	"uint8":   "readUInt8",
+	"uint16":  "readUInt16LE",
+	"uint32":  "readUInt32LE",
+	"uint64":  "readBigUInt64LE",
+	"float32": "readFloatLE",
+	"float64": "readDoubleLE",
 }
 
 var marshalMap = map[string]string{
-	"int8":   "writeInt8",
-	"int16":  "writeInt16LE",
-	"int32":  "writeInt32LE",
-	"int64":  "writeBigInt64LE",
-	"uint8":  "writeUInt8",
-	"uint16": "writeUInt16LE",
-	"uint32": "writeUInt32LE",
-	"uint64": "writeBigUInt64LE",
+	"int8":    "writeInt8",
+	"int16":   "writeInt16LE",
+	"int32":   "writeInt32LE",
+	"int64":   "writeBigInt64LE",
+	"uint8":   "writeUInt8",
+	"uint16":  "writeUInt16LE",
+	"uint32":  "writeUInt32LE",
+	"uint64":  "writeBigUInt64LE",
+	"float32": "writeFloatLE",
+	"float64": "writeDoubleLE",
 }
 
 func buildNodeMethod(method *service.Method) *methodDesc {
